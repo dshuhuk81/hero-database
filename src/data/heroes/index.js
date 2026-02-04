e2c4763f1247c3502c5d023e8eb677038947c910
Daniel Schultheiß <dshuhuk@gmail.com>
2026-02-04 09:14:21 +0100

    Initial commit

---
+++ src/data/heroes/index.js
@@ -0,0 +1,24 @@
+import tefnut from "./tefnut.json";
+import poseidon from "./poseidon.json";
+import amunra from "./amunra.json";
+import anubis from "./anubis.json";
+import aquarius from "./aquarius.json";
+import ares from "./ares.json";
+import athena from "./athena.json";
+import artemis from "./artemis.json";
+import bastet from "./bastet.json";
+import aries from "./aries.json";
+
+
+export const heroes = [
+  tefnut,
+  poseidon,
+  amunra,
+  anubis,
+  aquarius,
+  ares,
+  artemis,
+  athena,
+  bastet,
+  aries
+];
