f2583e610c81d1b529b45fb53304746c816b17c0
Daniel Schultheiß <dshuhuk@gmail.com>
2026-02-05 14:49:23 +0100

    add github pages depley

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://dshuhuk81.github.io",
+  base: "/hero-database/",
+  output: "static",
+});
