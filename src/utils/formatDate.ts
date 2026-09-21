dc0eb8162d06c0c09ae9efca717895ffdf3906b2
Daniel Schultheiß <dshuhuk@gmail.com>
2026-09-21 08:42:26 +0200

    Aufräumen

--- src/utils/formatDate.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateStr: string): string {
-  const date = new Date(dateStr);
-  return date.toLocaleDateString('de-DE', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  });
-}
